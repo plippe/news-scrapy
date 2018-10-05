906dd763332e58b2cd19198ba08b2bff3cc24b17
Philippe Vinchon <p.vinchon@gmail.com>
2018-10-05 23:20:45 +0100

    Add full example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,14 @@
 package com.github.plippe.news.scrapy
 
-import cats.effect.IO
+import cats.effect.{IO, Effect}
 import cats.implicits._
+import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3URI}
 import org.http4s.client.blaze.Http1Client
 import org.http4s.Uri
 
 import com.github.plippe.news.scrapy.models.Link
-import com.github.plippe.news.scrapy.stores.HttpStore
-import com.github.plippe.news.scrapy.parsers.IndependentIeArticleListParser
+import com.github.plippe.news.scrapy.stores._
+import com.github.plippe.news.scrapy.parsers._
 
 object Main extends App { // IOApp {
   trait ExitCode
@@ -15,19 +16,48 @@ object Main extends App { // IOApp {
     case object Success extends ExitCode
   }
 
-  def run(args: List[String]): IO[ExitCode] = {
+  def run[F[_]: Effect](): F[ExitCode] = {
     val stream = for {
-      client <- Http1Client.stream[IO]()
+      client <- Http1Client.stream[F]()
+      httpStore = new HttpStore[F](client)
+      amazonS3Store = new AmazonS3Store[F](
+        AmazonS3ClientBuilder.defaultClient())
 
-      link = Link.Http(Uri.uri("https://www.independent.ie/news/"))
-      document <- new HttpStore[IO](client).read(link)
-      uris <- new IndependentIeArticleListParser[IO]().parse(document)
-      _ = println(s"IndependentIeArticleListPageParser - $uris")
+      _ = println("Read independent.ie from HTTP")
+      articleListSourceHttpUri = Uri.uri("https://www.independent.ie/news/")
+      articleListSourceHttpLink = Link.Http(articleListSourceHttpUri)
+      articleListSourceHttp <- httpStore.read(articleListSourceHttpLink)
+
+      _ = println("Write independent.ie to Amazon S3")
+      articleListAmazonS3Uri = new AmazonS3URI(
+        s"s3://plippe-us-east-1/independent.ie/news/index.html")
+      articleListAmazonS3Link = Link.AmazonS3(articleListAmazonS3Uri)
+      _ <- amazonS3Store.write(articleListAmazonS3Link, articleListSourceHttp)
+
+      _ = println("Read independent.ie from Amazon S3")
+      articleList <- amazonS3Store.read(articleListAmazonS3Link)
+
+      _ = println("Parse independent.ie for URIs")
+      articleUri <- new IndependentIeArticleListParser[F]().parse(articleList)
+      articleLink = Link.Http(articleUri)
+
+      _ = println(s"Read ${articleUri} from HTTP")
+      article <- httpStore.read(articleLink)
+
+      _ = println(s"Write ${articleUri} to Amazon S3")
+      articleAmazonS3Uri = new AmazonS3URI(
+        s"s3://plippe-us-east-1/independent.ie${articleUri.path}")
+      articleAmazonS3Link = Link.AmazonS3(articleAmazonS3Uri)
+      _ <- amazonS3Store.write(articleAmazonS3Link, article)
+
+      _ = println(s"Parse ${articleUri}")
+      document <- new IndependentIeArticleParser[F]().parse(article)
+
+      _ = println(s"Done ${articleUri}")
     } yield ()
 
-    locally(args)
     stream.compile.drain.as(ExitCode.Success)
   }
 
-  run(args.toList).unsafeRunSync()
+  run[IO]().unsafeRunSync()
 }
