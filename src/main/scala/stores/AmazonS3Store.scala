d29e6c6b1e6c07a1c2e674c30d969c8bda8cb018
Philippe Vinchon <p.vinchon@gmail.com>
2018-10-05 23:16:21 +0100

    Wrong s3 client used

--- src/main/scala/stores/AmazonS3Store.scala
+++ src/main/scala/stores/AmazonS3Store.scala
@@ -1,23 +1,23 @@
 package com.github.plippe.news.scrapy.stores
 
 import cats.ApplicativeError
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3
 import fs2.Stream
 
-import com.github.plippe.news.scrapy.models.Link.AmazonS3
+import com.github.plippe.news.scrapy.models.Link
 
-class AmazonS3Store[F[_]](client: AmazonS3Client)(
+class AmazonS3Store[F[_]](client: AmazonS3)(
     implicit F: ApplicativeError[F, Throwable])
-    extends Reader[F, AmazonS3]
-    with Writer[F, AmazonS3] {
+    extends Reader[F, Link.AmazonS3]
+    with Writer[F, Link.AmazonS3] {
 
-  def read(link: AmazonS3): Stream[F, String] = Stream.eval {
+  def read(link: Link.AmazonS3): Stream[F, String] = Stream.eval {
     F.catchNonFatal {
       client.getObjectAsString(link.uri.getBucket, link.uri.getKey)
     }
   }
 
-  def write(link: AmazonS3, document: String): Stream[F, AmazonS3] =
+  def write(link: Link.AmazonS3, document: String): Stream[F, Link.AmazonS3] =
     Stream.eval {
       val f = F.catchNonFatal {
         client.putObject(link.uri.getBucket, link.uri.getKey, document)
