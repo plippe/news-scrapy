37e8b90feca9f27d3d3330408b4bc29bae3a8d8e
Philippe Vinchon <p.vinchon@gmail.com>
2018-10-20 22:09:35 +0100

    Add Orchestrator task

---
+++ lambda/src/main/scala/tasks/Orchestrator.scala
@@ -0,0 +1,25 @@
+package com.github.plippe.news.scrapy.tasks
+
+import cats.effect._
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder
+import fs2.Stream
+import io.circe.generic.auto._
+import io.circe.syntax._
+
+import com.github.plippe.news.scrapy.models.Link
+import com.github.plippe.news.scrapy.triggers._
+import com.github.plippe.news.scrapy.Task
+
+object Orchestrator {
+
+  def stream[F[_]: Concurrent](): Stream[F, Unit] = {
+    def trigger(trigger: Trigger[F, Link.AwsLambda], task: Task): Stream[F, Unit] =
+      trigger.trigger(Link.AwsLambda("news-scrapy"), task.asJson.noSpaces)
+
+    val awsLambdaClient = AWSLambdaClientBuilder.defaultClient()
+    val awsLambdaTrigger = new AwsLambdaTrigger[F](awsLambdaClient)
+
+    trigger(awsLambdaTrigger, Task.IndependentIe)
+  }
+
+}
