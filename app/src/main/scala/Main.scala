054e185100d531df62df52c602ba9a4f144973aa
Philippe Vinchon <p.vinchon@gmail.com>
2018-11-11 11:33:56 +0000

    Use web page in main

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -2,12 +2,12 @@ package com.github.plippe.news.scrapy
 
 import cats.effect.{ConcurrentEffect, IO}
 import cats.implicits._
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import fs2.Stream
+import java.io.File
 import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.Uri
 import scala.concurrent.ExecutionContext
 
-import com.github.plippe.news.scrapy.models.Link
 import com.github.plippe.news.scrapy.stores._
 import com.github.plippe.news.scrapy.parsers._
 import com.github.plippe.news.scrapy.syntax._
@@ -17,55 +17,40 @@ object Main extends App {
   def run[F[_]: ConcurrentEffect](): F[Unit] = {
     val stream = for {
       client <- BlazeClientBuilder[F](ec).stream
-      httpStore = new HttpStore[F](client)
-      amazonS3Store = new AmazonS3Store[F](
-        AmazonS3ClientBuilder.defaultClient())
 
-      hardDriveStore = new HardDriveStore[F]
+      articleListParser = new IndependentIeArticleListParser[F]()
+      articleParser = new IndependentIeArticleParser[F]()
 
-      newsUrl = "https://www.irishexaminer.com/"
-      directory = "irishexaminer"
-      rootFile = "news.html"
+      webPageReader: WebPageReader[F] = new WebPageHttpStore[F](client)
+      webPageWriter: WebPageWriter[F] = new WebPageHardDriveStore[F](
+        new File("."))
 
-      _ = println("Read from HTTP")
-      articleListSourceHttpUri = Uri.uri("https://www.irishexaminer.com/")
-      articleListSourceHttpLink = Link.Http(articleListSourceHttpUri)
-      articleListSourceHttp <- httpStore.read(articleListSourceHttpLink).stream
+      articleListUri = Uri.uri("https://www.independent.ie")
 
-      _ = println(s"Write to Hard Drive - $newsUrl")
-      /*articleListAmazonS3Uri = new AmazonS3URI(
-        s"s3://plippe-us-east-1/independent.ie/news/index.html")*/
-      articleListHardDriveLink = Link.HardDrive(s"$directory/$rootFile")
-      _ <- hardDriveStore
-        .write(articleListHardDriveLink, articleListSourceHttp)
-        .stream
+      _ = println(s"Read ${articleListUri} from HTTP")
+      articleListWebPage <- webPageReader.read(articleListUri).stream
 
-      _ = println(s"Read from Hard Drive - $directory/$rootFile")
-      articleList <- hardDriveStore.read(articleListHardDriveLink).stream
+      _ = println(s"Write ${articleListUri} to Amazon S3")
+      _ <- webPageWriter.write(articleListWebPage).stream
 
-      _ = println(s"Parse $newsUrl for URIs")
-      articleUri <- new IrishExaminerComArticleListParser[F]()
-        .parse(articleList)
+      _ = println(s"Parse ${articleListUri} for URIs")
+      articleUri <- articleListParser
+        .parse(articleListWebPage.html)
         .stream
         .flatMap { uris =>
-          fs2.Stream.apply(uris: _*)
+          Stream.apply(uris: _*)
         }
 
-      articleLink = Link.Http(articleUri)
-
       _ = println(s"Read ${articleUri} from HTTP")
-      article <- httpStore.read(articleLink).stream
+      articleWebPage <- webPageReader.read(articleUri).stream
 
       _ = println(s"Write ${articleUri} to Amazon S3")
-      articleHardDriveLink = Link.HardDrive(s"$directory/${articleUri.path}")
-      _ <- hardDriveStore.write(articleHardDriveLink, article).stream
+      _ <- webPageWriter.write(articleWebPage).stream
 
       _ = println(s"Parse ${articleUri}")
-      document <- new IrishExaminerComArticleParser[F]().parse(article).stream
+      article <- articleParser.parse(articleWebPage.html).stream
 
-      _ = println(s"Done ${document}")
-
-      _ = println(s"Done ${articleUri}")
+      _ = println(s"Done ${article}")
     } yield ()
 
     stream.compile.drain.as(())
--- core/src/main/scala/stores/WebPageStore.scala
+++ core/src/main/scala/stores/WebPageStore.scala
@@ -3,8 +3,10 @@ package com.github.plippe.news.scrapy.stores
 import cats.effect.{Effect, Sync}
 import cats.implicits._
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3URI}
+import java.io.{File, FileWriter}
 import org.http4s.client.Client
 import org.http4s.{Request, Uri}
+import scala.io.Source
 
 import com.github.plippe.news.scrapy.models.WebPage
 
@@ -22,28 +24,56 @@ class WebPageHttpStore[F[_]: Sync](client: Client[F]) extends WebPageReader[F] {
 
 }
 
-class WebPageAwsS3Store[F[_]: Effect](client: AmazonS3,
-                                      awsS3UriPrefix: AmazonS3URI)
+class WebPageAwsS3Store[F[_]: Effect](client: AmazonS3, prefix: AmazonS3URI)
     extends WebPageReader[F]
     with WebPageWriter[F] {
 
-  def awsS3Uri(uri: Uri): AmazonS3URI = {
-    new AmazonS3URI(s"${awsS3UriPrefix}/${uri}")
+  def buildAwsS3Uri(prefix: AmazonS3URI, uri: Uri): AmazonS3URI = {
+    new AmazonS3URI(s"${prefix}/${uri}")
   }
 
   def read(uri: Uri): F[WebPage] =
     Effect[F].catchNonFatal {
-      val s3Uri = awsS3Uri(uri)
-      val html = client.getObjectAsString(s3Uri.getBucket, s3Uri.getKey)
+      val awsS3Uri = buildAwsS3Uri(prefix, uri)
+      val html = client.getObjectAsString(awsS3Uri.getBucket, awsS3Uri.getKey)
 
       WebPage(uri, html)
     }
 
   def write(webPage: WebPage): F[Unit] =
     Effect[F].catchNonFatal {
-      val s3Uri = awsS3Uri(webPage.uri)
-      client.putObject(s3Uri.getBucket, s3Uri.getKey, webPage.html)
+      val awsS3Uri = buildAwsS3Uri(prefix, webPage.uri)
+      client.putObject(awsS3Uri.getBucket, awsS3Uri.getKey, webPage.html)
       ()
+    }
+
+}
+
+class WebPageHardDriveStore[F[_]: Effect](prefix: File)
+    extends WebPageReader[F]
+    with WebPageWriter[F] {
+
+  def buildFile(prefix: File, uri: Uri): File = {
+    new File(prefix, uri.toString)
+  }
+
+  def read(uri: Uri): F[WebPage] =
+    Effect[F].catchNonFatal {
+      val file = buildFile(prefix, uri)
+      val html = Source.fromFile(file).getLines.mkString("\n")
+
+      WebPage(uri, html)
+    }
+
+  def write(webPage: WebPage): F[Unit] =
+    Effect[F].catchNonFatal {
+      val file = buildFile(prefix, webPage.uri)
+
+      file.getParentFile().mkdirs()
+
+      val writer = new FileWriter(file)
+      writer.write(webPage.html)
+      writer.close()
     }
 
 }
