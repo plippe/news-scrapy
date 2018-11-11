685744520dff4d552cf9827ebba0d79c1cd8d635
Philippe Vinchon <p.vinchon@gmail.com>
2018-11-11 09:35:19 +0000

    Remove date from WebPage

--- core/src/main/scala/models/WebPage.scala
+++ core/src/main/scala/models/WebPage.scala
@@ -1,6 +1,5 @@
 package com.github.plippe.news.scrapy.models
 
-import java.time.ZonedDateTime
 import org.http4s.Uri
 
-case class WebPage(html: String, readOn: Uri, readAt: ZonedDateTime)
+case class WebPage(uri: Uri, html: String)
